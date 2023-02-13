5cb02e2b93bdd2dc3960d04a3497775d16339d72
Filip Schramka <f_schramka@hotmail.com>
2023-02-13 18:58:22 +0100

    full output

--- ska-sdp-func/conda/build.sh
+++ ska-sdp-func/conda/build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 export SKA_SDP_FUNC_LIB_DIR=$CONDA_PREFIX/lib
-export CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCUDA_ARCH=8.0" 
 
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCUDA_ARCH=8.0 ..
+make
+make install
+cd ..
+
+export CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCUDA_ARCH=8.0" 
 $PYTHON -m pip install . --no-deps
