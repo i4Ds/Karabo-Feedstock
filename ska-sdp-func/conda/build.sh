2fb19eb94565dc46465837701b16b26c9b0307fe
Filip Schramka <f_schramka@hotmail.com>
2023-02-13 20:06:05 +0100

    re added host removed second build path

--- ska-sdp-func/conda/build.sh
+++ ska-sdp-func/conda/build.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
 export SKA_SDP_FUNC_LIB_DIR=$CONDA_PREFIX/lib
-
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX ..
-make
-make install
-cd ..
-
 export CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX" 
 $PYTHON -m pip install . --no-deps
