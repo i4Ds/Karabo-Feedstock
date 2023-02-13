17b5417e2dae123faa0cde8961c46e1f16d87e00
Filip Schramka <f_schramka@hotmail.com>
2023-02-13 18:29:49 +0100

    the python process should call the one above...

--- ska-sdp-func/conda/build.sh
+++ ska-sdp-func/conda/build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-export CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCUDA_ARCH=8.0" 
 export SKA_SDP_FUNC_LIB_DIR=$CONDA_PREFIX/lib
 
-# install
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCUDA_ARCH=8.0 ..
+make
+make install
+cd ..
+
+export CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCUDA_ARCH=8.0" 
 $PYTHON -m pip install . --no-deps
