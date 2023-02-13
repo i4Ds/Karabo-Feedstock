3b8732b65b488fe2334fefd5f30c59f59b92206a
Filip Schramka <f_schramka@hotmail.com>
2023-02-13 19:13:14 +0100

    remove cuda arch 8.0

--- ska-sdp-func/conda/build.sh
+++ ska-sdp-func/conda/build.sh
@@ -4,10 +4,10 @@ export SKA_SDP_FUNC_LIB_DIR=$CONDA_PREFIX/lib
 
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCUDA_ARCH=8.0 ..
+cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX ..
 make
 make install
 cd ..
 
-export CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCUDA_ARCH=8.0" 
+export CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX" 
 $PYTHON -m pip install . --no-deps
