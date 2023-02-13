cd429475a1a3ec15d04c01ae8c90a38f73f7ae19
Filip Schramka <f_schramka@hotmail.com>
2023-02-13 18:44:33 +0100

    added older compiler

--- ska-sdp-func/conda/build.sh
+++ ska-sdp-func/conda/build.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 export SKA_SDP_FUNC_LIB_DIR=$CONDA_PREFIX/lib
-
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCUDA_ARCH=8.0 ..
-make
-make install
-cd ..
-
 export CMAKE_ARGS="-DCMAKE_INSTALL_PREFIX=$CONDA_PREFIX -DCUDA_ARCH=8.0" 
+
 $PYTHON -m pip install . --no-deps
