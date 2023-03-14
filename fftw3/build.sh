679bc585b59e32f2beaf877f22e8c7088a1734c9
Filip Schramka <f_schramka@hotmail.com>
2023-03-15 00:26:02 +0100

    also build single precision version

--- fftw3/build.sh
+++ fftw3/build.sh
@@ -4,6 +4,7 @@ COMPILER_PATH="$BUILD_PREFIX/bin"
 
 ./configure --prefix=$PREFIX --enable-mpi --enable-openmp --enable-threads \
 							 --enable-avx --enable-avx2 --enable-shared \
+							 --enable-float \
   CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc" CFLAGS="-O3" FFLAGS="-O3"
 
 make
