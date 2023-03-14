122e3c70aecb84291ddf358af258f2e8fd785494
Filip Schramka <filip.schramka@fhnw.ch>
2023-03-14 11:28:54 +0100

    add threading and avx support to fftw

--- fftw3/build.sh
+++ fftw3/build.sh
@@ -2,7 +2,9 @@
 
 COMPILER_PATH="$BUILD_PREFIX/bin"
 
-./configure --prefix=$PREFIX --enable-mpi --enable-openmp --disable-shared  CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc" CFLAGS="-O3" FFLAGS="-O3"
+./configure --prefix=$PREFIX --enable-mpi --enable-openmp --enable-threads \
+							 --disable-shared --enable-avx --enable-avx2 \
+  CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc" CFLAGS="-O3" FFLAGS="-O3"
 
 make
 make install
