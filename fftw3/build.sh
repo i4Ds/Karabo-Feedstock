2190fc81aae83f7287e5d44facab7c2a1a888749
Filip Schramka <filip.schramka@fhnw.ch>
2023-03-14 15:34:14 +0100

    enable shared objects

--- fftw3/build.sh
+++ fftw3/build.sh
@@ -3,7 +3,7 @@
 COMPILER_PATH="$BUILD_PREFIX/bin"
 
 ./configure --prefix=$PREFIX --enable-mpi --enable-openmp --enable-threads \
-							 --enable-avx --enable-avx2 \
+							 --enable-avx --enable-avx2 --enable-shared \
   CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc" CFLAGS="-O3" FFLAGS="-O3"
 
 make
