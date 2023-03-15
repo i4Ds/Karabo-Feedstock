9ebdc91e16f24e5a812000358f7b2035dca2e026
Filip Schramka <f_schramka@hotmail.com>
2023-03-15 01:34:19 +0100

    add both paths

--- fftw3/build.sh
+++ fftw3/build.sh
@@ -2,6 +2,15 @@
 
 COMPILER_PATH="$BUILD_PREFIX/bin"
 
+# double as base for the lib
+./configure --prefix=$PREFIX --enable-mpi --enable-openmp --enable-threads \
+							 --enable-avx --enable-avx2 --enable-shared \
+  CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc" CFLAGS="-O3" FFLAGS="-O3"
+
+make
+make install
+
+# float instead of double
 ./configure --prefix=$PREFIX --enable-mpi --enable-openmp --enable-threads \
 							 --enable-avx --enable-avx2 --enable-shared \
 							 --enable-float \
