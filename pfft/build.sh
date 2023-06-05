00b55589452806ed233071f84d41515222f3e91c
Filip Schramka <filip.schramka@fhnw.ch>
2023-06-05 17:33:35 +0200

    go for max avx2 arch

--- pfft/build.sh
+++ pfft/build.sh
@@ -3,7 +3,7 @@
 COMPILER_PATH="$BUILD_PREFIX/bin"
 
 ./bootstrap.sh
-./configure --prefix=$PREFIX CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc"
+./configure --prefix=$PREFIX CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc" CFLAGS="-O3 -march=skylake" FFLAGS="-O3"
 
 make
 make install
