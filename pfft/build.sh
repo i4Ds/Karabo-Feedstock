da1efb8733850442dc593ebc00ac29309108d7bc
Filip Schramka <filip.schramka@fhnw.ch>
2023-06-05 19:07:33 +0200

    did not take cflag - try it with enable-shared

--- pfft/build.sh
+++ pfft/build.sh
@@ -3,7 +3,9 @@
 COMPILER_PATH="$BUILD_PREFIX/bin"
 
 ./bootstrap.sh
-./configure --prefix=$PREFIX CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc" CFLAGS="-O3 -march=skylake" FFLAGS="-O3"
+./configure --prefix=$PREFIX --enable-mpi --enable-openmp --enable-threads \
+							 --enable-avx --enable-avx2 --enable-shared \
+  CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc" CFLAGS="-O3" FFLAGS="-O3"
 
 make
 make install
