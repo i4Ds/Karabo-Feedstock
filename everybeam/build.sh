2688e966111a60aa815a92c42293ca450674a97f
Delberin Ali <delberin.ali@fhnw.ch>
2024-10-17 13:37:10 +0200

    added fpermissive

--- everybeam/build.sh
+++ everybeam/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+#cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_CXX_FLAGS="-fpermissive" ..
 make install
