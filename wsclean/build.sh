3a4d1599922867ffd5203b64b4588ae961490bf1
d3v-null <dev.null@curtin.edu.au>
2025-07-03 07:18:57 +0000

    wsclean: gcc11 std=c++17

--- wsclean/build.sh
+++ wsclean/build.sh
@@ -2,6 +2,7 @@
 
 mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX ..
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX .. \
+    -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON
 make -j 4
 make install
