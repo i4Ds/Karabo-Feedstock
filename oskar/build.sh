c318c0f4cffca0468c8530c542c20c3659bd2cd5
Delberin Ali <delberin.ali@fhnw.ch>
2026-09-15 15:18:15 +0200

    fix(oskar): target portable glibc sysroot

--- oskar/build.sh
+++ oskar/build.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
+set -eux
 
-cmake -DCFIND_CUDA=ON -DCMAKE_INSTALL_PREFIX=$PREFIX -DCUDA_ARCH="6.0;6.1;6.2;7.0;7.5;8.0;8.6;8.7" \
-    -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED=ON
+echo "CC=${CC}"
+echo "CXX=${CXX}"
+"${CC}" --version
+"${CXX}" --version
+
+cmake ${CMAKE_ARGS:-} \
+    -DCFIND_CUDA=ON \
+    -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
+    -DCMAKE_C_COMPILER="${CC}" \
+    -DCMAKE_CXX_COMPILER="${CXX}" \
+    -DCUDA_ARCH="6.0;6.1;6.2;7.0;7.5;8.0;8.6;8.7" \
+    -DCMAKE_CXX_STANDARD=17 \
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON
+
 make -j2 install
