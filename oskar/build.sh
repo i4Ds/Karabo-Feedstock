31d408532222d9dd60b814bfc824ede9faef70b0
Delberin Ali <delberin.ali@fhnw.ch>
2026-09-15 16:02:58 +0200

    fix(oskar): support glibc 2.17 builds

--- oskar/build.sh
+++ oskar/build.sh
@@ -6,13 +6,16 @@ echo "CXX=${CXX}"
 "${CC}" --version
 "${CXX}" --version
 
-cmake ${CMAKE_ARGS:-} \
-    -DCFIND_CUDA=ON \
+cmake \
+    -DFIND_CUDA=ON \
+    -DCUDAToolkit_ROOT="${CUDA_PATH:-/usr/local/cuda}" \
+    -DCMAKE_PREFIX_PATH="${PREFIX}" \
     -DCMAKE_INSTALL_PREFIX="${PREFIX}" \
     -DCMAKE_C_COMPILER="${CC}" \
     -DCMAKE_CXX_COMPILER="${CXX}" \
     -DCUDA_ARCH="6.0;6.1;6.2;7.0;7.5;8.0;8.6;8.7" \
     -DCMAKE_CXX_STANDARD=17 \
-    -DCMAKE_CXX_STANDARD_REQUIRED=ON
+    -DCMAKE_CXX_STANDARD_REQUIRED=ON \
+    .
 
 make -j2 install
